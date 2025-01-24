fa6211ed946ae566a2400f93266d55c7faae17f2
Saim Momin <mominsaim12@gmail.com>
2025-01-24 10:50:37 +0100

    Replace file size test with image compare

--- pydeeptools/deeptools/test/test_fingerprintplot.py
+++ pydeeptools/deeptools/test/test_fingerprintplot.py
@@ -2,6 +2,7 @@ import deeptools.plotFingerprint as pf
 
 import os.path
 from os import unlink
+from matplotlib.testing.compare import compare_images
 
 
 ROOT = os.path.dirname(os.path.abspath(__file__)) + "/test_data/"
@@ -18,11 +19,8 @@ def test_fingerprint_plot_with_minimal_options():
     args = "-b {} {} -o {}".format(BAM_IN1, BAM_IN1, out_png).split()
     pf.main(args)
 
-    png_file_size = os.path.getsize(FIN_PLOT1)
-    expected_file_size = os.path.getsize(out_png)
-    size_tolerance = 15000
-    size_difference = abs(png_file_size - expected_file_size)
-    assert size_difference <= size_tolerance, "File size do not match"
+    res = compare_images(FIN_PLOT1, out_png, 50)
+    assert res is None, "Plots do not match"
     unlink(out_png)
 
 
@@ -33,24 +31,21 @@ def test_fingerprint_plot_with_advance_options():
     out_rawcounts = '/tmp/rawcounts.tsv'
     out_qualitymetric = '/tmp/qualitymetrics.tsv'
     out_png2 = '/tmp/fingerprint_plot2.png'
-    args = "-b {} {} -o {} --outRawCounts {} --outQualityMetrics {} -T Test_Fingerpring_Plot --JSDsample {}".format(BAM_IN1, BAM_IN1, out_png2, out_rawcounts, out_qualitymetric, BAM_IN1).split()
+    args = "-b {0} {0} -o {1} --outRawCounts {2} --outQualityMetrics {3} -T Test_Fingerpring_Plot --JSDsample {0}".format(BAM_IN1, out_png2, out_rawcounts, out_qualitymetric).split()
     pf.main(args)
 
-    with open(out_rawcounts, "r") as _foo:
-        result = len(_foo.readlines())
-    _expected = 16072
-    assert result == _expected, "No of lines in rawcounts files differ"
+    _foo = open(out_rawcounts, "r")
+    result = _foo.readlines()[2:7]
+    _expected = ["1327\t1327\n", "2015\t2015\n", "2928\t2928\n", "4419\t4419\n","8644\t8644\n"]
+    assert result == _expected, "Contens in rawcounts files differ"
 
     with open(out_qualitymetric, "r") as _foo:
         result2 = len(_foo.readlines())
     _expected = 3
     assert result2 == _expected, "No of lines in quality metrics files differ"
 
-    png_file_size = os.path.getsize(FIN_PLOT2)
-    expected_file_size = os.path.getsize(out_png2)
-    size_tolerance = 15000
-    size_difference = abs(png_file_size - expected_file_size)
-    assert size_difference <= size_tolerance, "File sizes do not match"
+    res = compare_images(FIN_PLOT2, out_png2, 50)
+    assert res is None, "Plots do not match"
     unlink(out_png2)
     unlink(out_rawcounts)
-    unlink(out_rawcounts)
+    unlink(out_qualitymetric)
